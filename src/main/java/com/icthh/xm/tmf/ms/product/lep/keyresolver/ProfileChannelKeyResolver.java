7d0181202d2a199ef3bb1a645459df942e2013df
Igor Dumchykov <igor.dumchykov@gmail.com>
2020-01-03 14:35:06 +0200

    Reimplement profile-channel keyresolver

--- src/main/java/com/icthh/xm/tmf/ms/product/lep/keyresolver/ProfileChannelKeyResolver.java
+++ src/main/java/com/icthh/xm/tmf/ms/product/lep/keyresolver/ProfileChannelKeyResolver.java
@@ -4,19 +4,27 @@ import com.icthh.xm.commons.lep.AppendLepKeyResolver;
 import com.icthh.xm.lep.api.LepManagerService;
 import com.icthh.xm.lep.api.LepMethod;
 import com.icthh.xm.lep.api.commons.SeparatorSegmentedLepKey;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
+@Slf4j
 public class ProfileChannelKeyResolver extends AppendLepKeyResolver {
 
     @Override
     protected String[] getAppendSegments(SeparatorSegmentedLepKey baseKey,
                                          LepMethod method,
                                          LepManagerService managerService) {
-        String product = getRequiredParam(method, "profile", String.class);
-        String channelId = getRequiredParam(method, "channelId", String.class);
-        product = translateToLepConvention(product);
+        HttpServletRequest request =
+            ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        String profile = request.getHeader("profile");
+        String channelId = request.getHeader("channel.id");
+        log.debug("Getting profile: {}, channelId: {} from request headers", profile, channelId);
+        profile = translateToLepConvention(profile);
         channelId = translateToLepConvention(channelId);
-        return new String[]{product, channelId};
+        return new String[]{profile, channelId};
     }
 }
--- src/main/java/com/icthh/xm/tmf/ms/product/web/rest/ProductDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/product/web/rest/ProductDelegate.java
@@ -8,49 +8,21 @@ import com.icthh.xm.tmf.ms.product.web.api.ProductApiDelegate;
 import com.icthh.xm.tmf.ms.product.web.api.model.Product;
 import io.micrometer.core.annotation.Timed;
 import java.util.List;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.ServletWebRequest;
 
 @Component
-@Slf4j
 @LepService(group = "service")
 public class ProductDelegate implements ProductApiDelegate {
 
-    private final HttpServletRequest request;
-
-    @Autowired
-    public ProductDelegate(final HttpServletRequest request) {
-        this.request = request;
-    }
-
-    @Override
-    public Optional<NativeWebRequest> getRequest() {
-        return Optional.of(new ServletWebRequest(request));
-    }
-
+    @LogicExtensionPoint(value = "GetProducts", resolver = ProfileChannelKeyResolver.class)
     @Timed
     @PreAuthorize("hasPermission({'profile': #profile}, 'PRODUCT.GET-LIST')")
     @Override
     public ResponseEntity<List<Product>> listProduct(String fields,
                                                      Integer offset,
                                                      Integer limit) {
-        return getRequest().map(req -> {
-            log.info("Native request {}", req.getNativeRequest());
-            String profile = req.getHeader("profile");
-            String channelId = req.getHeader("channel.id");
-            return listProduct(profile, channelId);
-        }).orElse(ResponseEntity.ok(ImmutableList.of(new Product())));
-    }
-
-    @LogicExtensionPoint(value = "GetProducts", resolver = ProfileChannelKeyResolver.class)
-    private ResponseEntity<List<Product>> listProduct(String profile, String channelId) {
         return ResponseEntity.ok(ImmutableList.of(new Product()));
     }
 }
